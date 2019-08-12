1758cbd2fb653ec921b60e1d566798d0455b7a7d
Max Silver <Maxbsilver@gmail.com>
2019-08-12 09:39:33 -0400

    add additional palindrom solution with array prototype every. This solution is less effective than the original due to over iteration

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -8,11 +8,16 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
-  // reverse the string
-  let rev = str.split('').reverse().join('');
-  // compare the original string against the reversed string
-  return rev === str ? true : false;
-  // if it is a palindrome return true
+  // // reverse the string
+  // let rev = str.split('').reverse().join('');
+  // // compare the original string against the reversed string
+  // return rev === str ? true : false;
+  // // if it is a palindrome return true
+  let arr = str.split('');
+
+  return arr.every((char, i) => {
+    return char === arr[arr.length-i-1]
+  })
 
 }
 
