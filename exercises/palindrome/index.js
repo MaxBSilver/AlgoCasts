58a8f41f61694fa6c8438db41d2cf0244132ac6c
Max Silver <Maxbsilver@gmail.com>
2019-08-12 09:29:23 -0400

    Add palindrome solution

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,13 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  // reverse the string
+  let rev = str.split('').reverse().join('');
+  // compare the original string against the reversed string
+  return rev === str ? true : false;
+  // if it is a palindrome return true
+
+}
 
 module.exports = palindrome;
